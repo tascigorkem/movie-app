164d26bf768c406270b8f7819a68d5c84f8b4d90
Görkem Taşçı <gorkem_tasci@hotmail.com>
2020-04-17 22:34:16 +0300

    Updates

---
+++ precache-manifest.ed560111ecbb2fc074b0f17db892be12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f13395c9e42d51f8bfe0afc880ebc47",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "b257a352a214ea54e3b8",
+    "url": "/movie-app/static/css/main.069965bb.chunk.css"
+  },
+  {
+    "revision": "06f832e609cb2a7cfc4f",
+    "url": "/movie-app/static/js/2.79015ea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.79015ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b257a352a214ea54e3b8",
+    "url": "/movie-app/static/js/main.e9dce627.chunk.js"
+  },
+  {
+    "revision": "ef13b2d46e7e451c8c9d",
+    "url": "/movie-app/static/js/runtime-main.4744ba8c.js"
+  }
+]);
